dc4ca4684a2770f39d21809e4fd14e7b9d862f63
wisnuwirayuda <wisnu.wirayuda01@gmail.com>
2025-02-23 21:47:42 +0700

    How to Create Hello Vue File

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,13 @@
+<script setup>
+console.log('Hello Vue');
+</script>
+
+<template>
+    <h1 class="hello">Hello Vue</h1>
+</template>
+
+<style scoped>
+.hello {
+    color: green;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        hello: "hello.html"
+      }
+    }
+  }
 })
